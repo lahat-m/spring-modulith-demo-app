e8a93e60cd3bbdc4524118fbb41913b34d6a9e70
Lahat Mariel <67752087+lahat-m@users.noreply.github.com>
2025-12-27 12:10:04 +0300

    for fun updated main method to use void main for the Java 25.

--- src/main/java/com/lahat/springmodulith/SpringModulithApplication.java
+++ src/main/java/com/lahat/springmodulith/SpringModulithApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringModulithApplication {
 
-	public static void main(String[] args) {
+	void main(String[] args) {
 		SpringApplication.run(SpringModulithApplication.class, args);
 	}
 
